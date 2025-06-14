9282e4af33018d517f9c6b0b30e86ce272eb0598
nodeluna <nodeluna@proton.me>
2025-06-14 15:32:55 +0300

    added a unit test

--- tests/test.cpp
+++ tests/test.cpp
@@ -397,3 +397,118 @@ TEST_F(ljson_test, node_plus_node_arrays)
 	EXPECT_TRUE(new_node.is_array());
 	EXPECT_EQ(new_node.as_array()->size(), array_node1.as_array()->size() + array_node2.as_array()->size());
 }
+
+TEST_F(ljson_test, insert_into_object)
+{
+	ljson::node node = {
+	    {"key1", "value1"},
+	    {"key2", "value2"},
+	};
+
+	std::map<std::string, int> object = {
+		{"key1", 1},
+		{"key2", 2},
+	};
+
+	std::set<std::string> array = {"arr1", "arr2"};
+
+	node.insert("key3", (const char*) "value3");
+	node.insert("key4", (const char*) "value4");
+	node.insert("arr", array);
+	node.insert("obj", object);
+
+	EXPECT_TRUE(node.contains("key1"));
+	EXPECT_TRUE(node.at("key1").is_value());
+	EXPECT_TRUE(node.at("key1").as_value()->is_string());
+	EXPECT_EQ(node.at("key1").as_value()->as_string(), "value1");
+
+	EXPECT_TRUE(node.contains("key2"));
+	EXPECT_TRUE(node.at("key2").is_value());
+	EXPECT_TRUE(node.at("key2").as_value()->is_string());
+	EXPECT_EQ(node.at("key2").as_value()->as_string(), "value2");
+
+	EXPECT_TRUE(node.contains("key3"));
+	EXPECT_TRUE(node.at("key3").is_value());
+	EXPECT_TRUE(node.at("key3").as_value()->is_string());
+	EXPECT_EQ(node.at("key3").as_value()->as_string(), "value3");
+
+	EXPECT_TRUE(node.contains("key4"));
+	EXPECT_TRUE(node.at("key4").is_value());
+	EXPECT_TRUE(node.at("key4").as_value()->is_string());
+	EXPECT_EQ(node.at("key4").as_value()->as_string(), "value4");
+
+	EXPECT_TRUE(node.contains("arr"));
+	EXPECT_TRUE(node.at("arr").is_array());
+
+	EXPECT_TRUE(node.at("arr").at(0).is_value());
+	EXPECT_TRUE(node.at("arr").at(0).as_value()->is_string());
+	EXPECT_EQ(node.at("arr").at(0).as_value()->as_string(), "arr1");
+
+	EXPECT_TRUE(node.at("arr").at(1).is_value());
+	EXPECT_TRUE(node.at("arr").at(1).as_value()->is_string());
+	EXPECT_EQ(node.at("arr").at(1).as_value()->as_string(), "arr2");
+
+	EXPECT_TRUE(node.contains("obj"));
+	EXPECT_TRUE(node.at("obj").is_object());
+
+	EXPECT_TRUE(node.at("obj").contains("key1"));
+	EXPECT_TRUE(node.at("obj").at("key1").is_value());
+	EXPECT_TRUE(node.at("obj").at("key1").as_value()->is_number());
+	EXPECT_EQ(node.at("obj").at("key1").as_value()->as_number(), 1);
+
+	EXPECT_TRUE(node.at("obj").contains("key2"));
+	EXPECT_TRUE(node.at("obj").at("key2").is_value());
+	EXPECT_TRUE(node.at("obj").at("key2").as_value()->is_number());
+	EXPECT_EQ(node.at("obj").at("key2").as_value()->as_number(), 2);
+}
+
+TEST_F(ljson_test, push_back_into_array)
+{
+	ljson::node node(ljson::value_type::array);
+
+	std::map<std::string, int> object = {
+		{"key1", 1},
+		{"key2", 2},
+	};
+
+	std::set<std::string> array = {"arr1", "arr2", "arr3"};
+
+	node.push_back((const char*) "value1");
+	node.push_back((const char*) "value2");
+	node.push_back(array);
+	node.push_back(object);
+
+	EXPECT_TRUE(node.at(0).is_value());
+	EXPECT_TRUE(node.at(0).as_value()->is_string());
+	EXPECT_EQ(node.at(0).as_value()->as_string(), "value1");
+
+	EXPECT_TRUE(node.at(1).is_value());
+	EXPECT_TRUE(node.at(1).as_value()->is_string());
+	EXPECT_EQ(node.at(1).as_value()->as_string(), "value2");
+
+	EXPECT_TRUE(node.at(2).is_array());
+
+	EXPECT_TRUE(node.at(2).as_array()->at(0).is_value());
+	EXPECT_TRUE(node.at(2).as_array()->at(0).as_value()->is_string());
+	EXPECT_EQ(node.at(2).as_array()->at(0).as_value()->as_string(), "arr1");
+
+	EXPECT_TRUE(node.at(2).as_array()->at(1).is_value());
+	EXPECT_TRUE(node.at(2).as_array()->at(1).as_value()->is_string());
+	EXPECT_EQ(node.at(2).as_array()->at(1).as_value()->as_string(), "arr2");
+
+	EXPECT_TRUE(node.at(2).as_array()->at(2).is_value());
+	EXPECT_TRUE(node.at(2).as_array()->at(2).as_value()->is_string());
+	EXPECT_EQ(node.at(2).as_array()->at(2).as_value()->as_string(), "arr3");
+
+	EXPECT_TRUE(node.at(3).is_object());
+
+	EXPECT_TRUE(node.at(3).contains("key1"));
+	EXPECT_TRUE(node.at(3).at("key1").is_value());
+	EXPECT_TRUE(node.at(3).at("key1").as_value()->is_number());
+	EXPECT_EQ(node.at(3).as_object()->at("key1").as_value()->as_number(), 1);
+
+	EXPECT_TRUE(node.at(3).contains("key2"));
+	EXPECT_TRUE(node.at(3).at("key2").is_value());
+	EXPECT_TRUE(node.at(3).at("key2").as_value()->is_number());
+	EXPECT_EQ(node.at(3).as_object()->at("key2").as_value()->as_number(), 2);
+}
